3a73a5b2294c46b85f0d8352b95f6e35675cf95d
Doc Walker <4-20ma@wvfans.net>
2014-05-07 05:57:37 -0500

    Bump version, update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 CHANGELOG
 =========
 
+0.1.1 / 2014-05-07
+------------------
+
+- `IMPROVE` - convert [README](README.md) recipe list to table
+
+
 0.1.0 / 2014-05-06
 ------------------
 
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email  '4-20ma@wvfans.net'
 description       'Creates rpm package for postfix.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license           'Apache 2.0'
-version           '0.1.0'
+version           '0.1.1'
 
 #------------------------------------------------------- cookbook dependencies
 depends           'yum-epel', '~> 0.3.6'
