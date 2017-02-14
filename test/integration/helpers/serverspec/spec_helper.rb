898f849342719be6b27acc811801d639ceb6e3be
Doc Walker <4-20ma@wvfans.net>
2017-02-13 20:49:24 -0600

    Remove deprecated include statement

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -2,8 +2,7 @@
 require 'serverspec'
 require 'platform_helpers'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
